e3186153f0b136b13e724a33614217908c0f4d26
benarmstrong1616 <ben.armstrong@live.co.uk>
2020-05-13 22:27:23 +0100

    commit

--- js/contact-form-script.js
+++ js/contact-form-script.js
@@ -1,55 +1,48 @@
-$("#contactForm").validator().on("submit", function (event) {
-    if (event.isDefaultPrevented()) {
-        // handle the invalid form...
-        formError();
-        submitMSG(false, "Did you fill in the form properly?");
-    } else {
-        // everything looks good!
-        event.preventDefault();
-        submitForm();
+<script>
+
+    var form_id_js = "contactForm";
+
+    var data_js = {
+        "access_token": "x30jltu12txe7ol20xw2c16s"
+    };
+
+    var sendButton = document.getElementById("email_submit");
+
+    function js_send() {
+        sendButton.value='Sending…';
+        sendButton.disabled=true;
+        var request = new XMLHttpRequest();
+
+        var subject = document.querySelector("#" + form_id_js + " [name='email_subject']").value;
+        var message = document.querySelector("#" + form_id_js + " [name='email_message']").value;
+        var number = document.querySelector("#" + form_id_js + " [name='email_number']").value;
+        var email_address = document.querySelector("#" + form_id_js + " [name='email_address']").value;
+        var name = document.querySelector("#" + form_id_js + " [name='email_name']").value;
+        data_js['subject'] = subject;
+        data_js['text'] = "Sent by: " + name + ", Email: " + email_address + ", Number: " + number + "\n" + message;
+        var params = toParams(data_js);
+
+        request.open("POST", "https://postmail.invotes.com/send", true);
+        request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+        request.send(params);
+
+        return false;
     }
-});
 
+    sendButton.onclick = js_send;
 
-function submitForm(){
-    // Initiate Variables With Form Content
-    var name = $("#name").val();
-    var email = $("#email").val();
-    var msg_subject = $("#msg_subject").val();
-    var message = $("#message").val();
-
-
-    $.ajax({
-        type: "POST",
-        url: "php/form-process.php",
-        data: "name=" + name + "&email=" + email + "&msg_subject=" + msg_subject + "&message=" + message,
-        success : function(text){
-            if (text == "success"){
-                formSuccess();
-            } else {
-                formError();
-                submitMSG(false,text);
-            }
+    function toParams(data_js) {
+        var form_data = [];
+        for ( var key in data_js ) {
+            form_data.push(encodeURIComponent(key) + "=" + encodeURIComponent(data_js[key]));
         }
-    });
-}
 
-function formSuccess(){
-    $("#contactForm")[0].reset();
-    submitMSG(true, "Message Submitted!")
-}
-
-function formError(){
-    $("#contactForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-        $(this).removeClass();
-    });
-}
-
-function submitMSG(valid, msg){
-    if(valid){
-        var msgClasses = "h3 text-center tada animated text-success";
-    } else {
-        var msgClasses = "h3 text-center text-danger";
+        return form_data.join("&");
     }
-    $("#msgSubmit").removeClass().addClass(msgClasses).text(msg);
-}
+
+    var js_form = document.getElementById(form_id_js);
+    js_form.addEventListener("submit", function (e) {
+        e.preventDefault();
+    });
+</script>
